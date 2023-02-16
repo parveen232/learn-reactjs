1ab89455a4dcf60dce105bd357388726150e6d63
parveen232 <parveen297m210@gmail.com>
2023-02-16 21:33:00 +0530

    tic-tac-toe

---
+++ Tic-Tac-Toe/src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Game from "./Game";
+import "./styles.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Game />
+  </StrictMode>
+);
